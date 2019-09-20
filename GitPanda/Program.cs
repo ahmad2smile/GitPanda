e986c56e93489c44a3b9d8a5e7868adb2613d562
Ahmad <ahmad2smile@protonmail.com>
2019-09-20 17:10:16 +0700

    Add project files.

---
+++ GitPanda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitPanda
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Arg: {arg}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
